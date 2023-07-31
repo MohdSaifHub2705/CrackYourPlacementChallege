bbb0e8ca1e453ebef5b1a209db3d5d20748e36fa
Mohd Saif <106275738+MohdSaifHub2705@users.noreply.github.com>
2023-07-31 20:47:46 +0530

    Create FindAllDuplicate.java

---
+++ Arrays/FindAllDuplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> al = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+              al.add(nums[i]);
+            }
+        }
+        return al;
+    }
+}
