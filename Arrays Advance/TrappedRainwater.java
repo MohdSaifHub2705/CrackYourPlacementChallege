933745aebc714c70c78a786d183dd02fb33e1b17
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2023-08-13 12:25:54 +0530

    Create TrappedRainwater.java

---
+++ Arrays Advance/TrappedRainwater.java
@@ -0,0 +1,38 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class HelloWorld {
+    // Trapped Rainwater Problem
+    public static int trappedRainWater(int[] height){
+       int n = height.length;
+    //calculate the left max boundary
+    int leftmax[] = new int[n];
+    leftmax[0] = height[0];
+    for(int i=1; i<n; i++) {
+        leftmax[i] = Math.max(height[i], leftmax[i-1]);
+    }
+    // calculate the right max boundary
+    int rightmax[] = new int[n];
+    rightmax[n-1] = height[n-1];
+    for(int i=n-2; i>=0; i--) {
+        rightmax[i] = Math.max(height[i], rightmax[i+1]);
+    }
+    
+    // take variable to store the trapped water
+    int trappedWater = 0;
+    // run a loop for height of array
+    for(int i=0; i<n; i++) {
+        // find the water level from left max and right max
+        int waterLevel = Math.min(leftmax[i], rightmax[i]);
+        // find trapped water volume 
+        trappedWater += waterLevel-height[i];
+    }
+    return trappedWater;
+  }
+  
+    public static void main(String[] args) {
+      int height[] = {4,2,0,6,3,2,5};
+       System.out.print("Maximum Trapped Water is : " +  trappedRainWater(height));
+    }
+}
