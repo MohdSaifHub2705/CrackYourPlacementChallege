332b96588592304008f9ff7700b9220c70d3e880
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2023-08-17 15:09:09 +0530

    Create StringCompression.java

---
+++ String/StringCompression.java
@@ -0,0 +1,25 @@
+class HelloWorld {
+    
+    public static String compression(String str) {
+        String newStr = "";
+        
+        for(int i=0; i<str.length(); i++) {
+            Integer count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if(count > 1) {
+              newStr += count.toString();  
+            }
+        }
+        return newStr;
+    }
+    
+    public static void main(String[] args) {
+    //   String str = "aaaabbbcdde";
+       String str = "abcdeee";
+       System.out.println("The Compression String is : " + compression(str));
+    }
+}
