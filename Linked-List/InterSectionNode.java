11a0e2568a646d7da97f8bc772ce4d83ec9a85e6
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2023-08-20 14:08:45 +0530

    Create InterSectionNode.java

---
+++ Linked-List/InterSectionNode.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+          ListNode ptrA =  headA;
+          ListNode ptrB = headB;
+
+          while(ptrA != ptrB) {
+            ptrA = ptrA == null ? headB : ptrA.next;
+            ptrB = ptrB == null ? headA : ptrB.next;
+          }
+          return ptrA;
+    }
+}
