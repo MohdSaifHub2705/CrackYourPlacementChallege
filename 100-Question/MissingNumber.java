79de86e90d1f056e57a870ea948168b1de098ea0
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2023-09-16 18:47:05 +0530

    Create MissingNumber.java

---
+++ 100-Question/MissingNumber.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      int arr[] = {5,3,2,1};
+      int n = arr.length+1;
+      int sum = n*(n+1)/2;
+      int restSum = 0;
+      for(int i=0; i<arr.length; i++) {
+          restSum += arr[i];
+      }
+      int missingNumber = sum-restSum;
+      
+      System.out.println("Missing Number is : " + missingNumber);
+    }
+}
