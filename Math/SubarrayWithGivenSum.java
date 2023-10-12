a6c5b5dd360b0ad69d2ef82e99a6367b61e8419f
Mohd Saif <106275738+saif0056@users.noreply.github.com>
2023-10-12 07:31:01 +0530

    Update SubarrayWithGivenSum.java

--- Math/SubarrayWithGivenSum.java
+++ Math/SubarrayWithGivenSum.java
@@ -31,3 +31,65 @@ class Arrays {
         arr.printFindSubArr(nums, n, sum);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sliding window algo
+
+
+
+class Subarray {
+    
+    static int[] findSubarrayWithSum(int[] nums, int targetSum) {
+        if(nums == null || nums.length == 0) {
+            return new int[] {-1,-1};
+        }
+        int left = 0;
+        int right = 0;
+        int currentSum = 0;
+        
+        while(right < nums.length) {
+            currentSum +=  nums[right];
+            
+            while(currentSum > targetSum && left <= right) {
+                // remove curent sum from left and move pointer
+                currentSum -= nums[left];
+                left++;
+            }
+            if(currentSum == targetSum) {
+                return new int[]{left, right};
+            }
+            right++;
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        int targetSum = 40;
+                int[] subarray = findSubarrayWithSum(nums, targetSum);
+
+        if (subarray[0] == -1) {
+            System.out.println("No subarray found with the given sum.");
+        } else {
+            System.out.println("Subarray with the sum " + targetSum + ":");
+            for (int i = subarray[0]; i <= subarray[1]; i++) {
+                System.out.print(nums[i] + " ");
+            }
+        }
+
+    }
+}
